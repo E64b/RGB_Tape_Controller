84061264ad378665ea5dac7a0ce3244e50ce0b08
Mike <125614854+E64b@users.noreply.github.com>
2023-10-07 19:39:33 +0300

    edit

--- test/test.ino
+++ test/test.ino
@@ -15,63 +15,63 @@
 #define G (CURRENT_PIN + 1)
 #define B (CURRENT_PIN + 2)
 
-GyverHC595<SHIFTS, HC_PINS> reg (STCP, DS, SHCP);
+GyverHC595<SHIFTS, HC_PINS> reg(STCP, DS, SHCP);
 
-uint16_t CURRENT_PIN = 0;
+uint16_t CURRENT_PIN=0;
 
-void setup (){
-	Serial.begin (9600);
-	while (!Serial);
-	Serial.print ("Serial OK");
+void setup(){
+	Serial.begin(9600);
+	while(!Serial);
+	Serial.print("Serial OK");
 
-	reg.clearAll ();
-	reg.update ();
-	reg.setAll ();
-	reg.update ();
-	delay (1000);
-	reg.clearAll ();
-	reg.update ();
+	reg.clearAll();
+	reg.update();
+	reg.setAll();
+	reg.update();
+	delay(1000);
+	reg.clearAll();
+	reg.update();
 	}
 
-void loop (){
+void loop(){
 	/*Turn on all the red */
-	CURRENT_PIN = 0;
-	for (R; R <= ALL_DATA; R + 3){
-		reg.set (R);
-		reg.update ();
-		delay (500);
+	CURRENT_PIN=0;
+	for(R; R<=ALL_DATA; R+3){
+		reg.set(R);
+		reg.update();
+		delay(500);
 		}
-	reg.clearAll ();
-	reg.update ();
+	reg.clearAll();
+	reg.update();
 
 	/*Turn on all the green */
-	CURRENT_PIN = 0;
-	for (G; G <= ALL_DATA; G + 3){
-		reg.set (G);
-		reg.update ();
-		delay (500);
+	CURRENT_PIN=0;
+	for(G; G<=ALL_DATA; G+3){
+		reg.set(G);
+		reg.update();
+		delay(500);
 		}
-	reg.clearAll ();
-	reg.update ();
+	reg.clearAll();
+	reg.update();
 
 	/*Turn on all the blue */
-	CURRENT_PIN = 0;
-	for (B; B <= ALL_DATA; B + 3){
-		reg.set (B);
-		reg.update ();
-		delay (500);
+	CURRENT_PIN=0;
+	for(B; B<=ALL_DATA; B+3){
+		reg.set(B);
+		reg.update();
+		delay(500);
 		}
-	reg.clearAll ();
-	reg.update ();
+	reg.clearAll();
+	reg.update();
 
 	/*Turn on all the white */
-	for (CURRENT_PIN = 0; CURRENT_PIN <= ALL_DATA; CURRENT_PIN + 3){
-		reg.set (R);
-		reg.set (G);
-		reg.set (B);
-		reg.update ();
-		delay (500);
+	for(CURRENT_PIN=0; CURRENT_PIN<=ALL_DATA; CURRENT_PIN+3){
+		reg.set(R);
+		reg.set(G);
+		reg.set(B);
+		reg.update();
+		delay(500);
 		}
-	reg.clearAll ();
-	reg.update ();
+	reg.clearAll();
+	reg.update();
 	}
