de79a91728e765a60102911f83cf0f7ada0b82e6
Mike <125614854+E64b@users.noreply.github.com>
2023-10-09 13:09:08 +0300

    edit

--- main/main.h
+++ main/main.h
@@ -2,20 +2,22 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-/*
-#define REG
-#define VAL
-#define R
-#define G
-#define B
-*/
 #define STCP 10 //pinCS 12
 #define DS 11 //pinDT 14
 #define SHCP 13 //pinCLK 11
-#define SHIFTS 6
+
+#define SHIFTS 1
 #define HC595_DELAY 0
+#define ONE_SHIFT_DATA 8
+#define ALL_DATA (SHIFTS * ONE_SHIFT_DATA)
+
+#define R CURRENT_PIN
+#define G (CURRENT_PIN + 1)
+#define B (CURRENT_PIN + 2)
 
 GyverHC595<SHIFTS, HC_PINS> reg(STCP, DS, SHCP);
+bool IN_VAL[ALL_DATA] {};
+bool OLD_VAL[ALL_DATA]{};
+uint16_t CURRENT_PIN=0;
 
-void read_seral();
-void send_val();
+extern CURRENT_PIN;
--- main/read_serial.cpp
+++ main/read_serial.cpp
@@ -1,4 +1,13 @@
 #include "main.h"
 void read_seral(){
-
-}
+	int i=0;
+	while(Serial.available()>0){
+		bool c=(char)Serial.read();
+		if(c=='!'){
+			sendData=true;
+			break;
+			}
+		arr[i]=c;
+		i++;
+		}
+	}
