35b234689522f437d7ddd2be7f825fc721846ceb
Mike <125614854+E64b@users.noreply.github.com>
2023-10-09 17:53:50 +0300

    v0.4

--- main/check.cpp
+++ main/check.cpp
@@ -3,10 +3,9 @@
 void check(){
   	if(uiState.recived){
 		for(uint16_t a = 0; a<ALL_DATA; a++){
-      bool var = uiState.IN_VAL[a]; 
-      Serial.println(var);
-			if(reg.buffer[a]!=var){
-				reg.write(a, var);
+        Serial.println(uiState.IN_VAL[a]);
+			if(reg.buffer[a]!= uiState.IN_VAL[a]){
+				reg.write(a, uiState.IN_VAL[a]);
 				uiState.send=true;
 				}
 			}
--- main/read_serial.cpp
+++ main/read_serial.cpp
@@ -3,10 +3,10 @@
 void read_serial(){
 	uint16_t i=0;
 	while(Serial.available()>0){
-		char VAL=(char)Serial.read();
-    uiState.IN_VAL[i]=VAL;
+		bool VAL=(bool)Serial.read();
+		uiState.IN_VAL[i]=VAL;
 		Serial.println(VAL);
-		if(VAL=='!'){
+		if(i==ALL_DATA){
 			Serial.println("rec");
 			uiState.recived=true;
 			break;
