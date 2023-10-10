d153906ba5c2c89e054b39de2f4399687958354c
Mike <125614854+E64b@users.noreply.github.com>
2023-10-10 15:44:32 +0300

    v1.2

--- main/check.cpp
+++ main/check.cpp
@@ -3,7 +3,7 @@
 void check(){
 	if(uiState.recived){
 		for(uint16_t i=0; i<ALL_DATA; i++){
-			if(reg.read(i)!=uiState.IN_VAL[i){
+			if(reg.read(i)!=uiState.IN_VAL[i]){
 				reg.write(i, uiState.IN_VAL[i]);
 				uiState.send=true;
 				}
